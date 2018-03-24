4abddcde04abc9f3dd00da8f49d0b2e78dcb9420
jose.arangos <jose.arangos@udea.edu.co>
2018-03-24 11:06:29 -0500

    front

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.listen(PORT,function(){
 
 app.get('/api/message',function(req, res){
     var nombre=req.param('name');
-    res.send('<center><h1>'+'hola: '+nombre+'</h1></center>');
+    res.send('<style>background-color: #f5f5f5;</style><center><h1>'+'hola: '+nombre+'</h1></center>');
 });
 
 
