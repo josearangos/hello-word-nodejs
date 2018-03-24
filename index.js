18c2cc6b21f9930c3033294a75edc70211255092
jose.arangos <jose.arangos@udea.edu.co>
2018-03-24 10:51:32 -0500

    despligue

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
-const path = require('path')
 const PORT = process.env.PORT || 5000
+var app=express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT,function(){
+    console.log("escuchando");
+})
+
+app.get('/api/message',function(req, res){
+    var nombre=req.param('name');
+    res.send('<h1>'+'hola: '+nombre+'</h1>');
+});
+
+
+
